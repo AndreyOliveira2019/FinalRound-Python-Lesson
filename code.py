20672dfbb3801aba9b3be3dc60ec314e88454f06
AndreyOliveira2019 <55673289+AndreyOliveira2019@users.noreply.github.com>
2021-05-14 18:15:42 -0300

    Update code.py

--- code.py
+++ code.py
@@ -17,4 +17,34 @@ print(media_da_lista)
 #Item 2 da Lista de exercícios - crie um método que receba duas matrizes, some os valores total de cada matriz e
 #depois multiple o resultado delas e retorne o valor.
 
+def lista(primeiramatriz, segundamatriz):
+
+  resultado1 = 0
+  resultado2 = 0
+  resultado3 = 0
+
+  for linha in primeiramatriz: 
+    for coluna in linha: 
+      resultado1 = coluna + resultado1
+
+  for linha2 in segundamatriz: 
+    for coluna in linha2:
+      resultado2 = coluna + resultado2
+
+  print('exibir resultado da soma da primeiramatriz:', resultado1)
+  print('exibir resultado da soma da segundamatriz:', resultado2)
+
+  resultado3 = resultado1 * resultado2
+  print('exibir resultado da multiplicação das duas matrizes:', resultado3)
+    
+  
+  primeiramatriz = [[1,2,3],[1,2,3]]
+  segundamatriz = [[1,2,3],[10,20,30]]
+
+lista(primeiramatriz, segundamatriz)
+
+#Criar uma matriz nxm (n = 5, m =7)
+
+
+
 
