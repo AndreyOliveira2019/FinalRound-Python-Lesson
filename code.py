96d029a7129512dc9e1e8c5d0e5e7446f1d9cc9b
AndreyOliveira2019 <55673289+AndreyOliveira2019@users.noreply.github.com>
2021-05-13 23:21:55 -0300

    Update code.py

--- code.py
+++ code.py
@@ -12,3 +12,9 @@ for lista in lista:
     soma_da_lista += lista  
 media_da_lista = soma_da_lista / 9
 print(media_da_lista)
+
+
+#Item 2 da Lista de exercícios - crie um método que receba duas matrizes, some os valores total de cada matriz e
+#depois multiple o resultado delas e retorne o valor.
+
+
